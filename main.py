fb5c6165f61e0fbbd138d8822f4d872a2d19dc32
hojo4646 <hojo4646@gmail.com>
2021-07-20 15:34:28 -0600

    First Functional Build
    Program can now download a youtube video from url from the GUI, after pressing download. I consider this our first deliverable application, as it can complete the simplest form of task needed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
 from PyQt5.uic import loadUi
 from gui import Ui_MainWindow
@@ -9,7 +10,20 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-    
+        self.ydl_opts = {}
+        self.button_url_download.clicked.connect(self.download_url)
+        
+
+    def get_url(self):
+        return self.input_url.text()
+
+    def test(self):
+        print(self.get_url())
+
+    def download_url(self):
+        with ydl.YoutubeDL(self.ydl_opts) as yt:
+            yt.download([self.get_url()])
+
     def get_url_info(self):
         pass
 
