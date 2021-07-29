015775551f21d13680bf9e294039593e9c5b0ddf
hojo4646 <hojo4646@gmail.com>
2021-07-28 18:06:05 -0600

    Added FFMPEG Functionality
    Can now download and convert multiple formats, audio and video. added progress bar, working on adding file save functionality

--- convert.bat
+++ convert.bat
@@ -1,1 +1,2 @@
 pyuic5 GUI.ui -o gui.py
+pyuic5 progressbar.ui -o progressbar.py
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
+import progressbar
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox, QWidget, QFileDialog
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.uic import loadUi
 from gui import Ui_MainWindow
@@ -13,18 +14,37 @@ import io
 import requests
 import matplotlib.pyplot as plt  #remove later 
 
+BIN = 'bin'
+
+class Progress(QWidget,progressbar.Ui_Form):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        
+
 
 class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        self.ydl_opts = {}
+        self.progress = Progress()
+        self.progressBar = self.progress.progressBar
 
-        # Connect the download button to the "download_url"
-        self.button_url_download.clicked.connect(self.download_url)
+
+        # Connect the download button to the "download_button"
+        self.button_url_download.clicked.connect(self.download_button)
         # Connect the url input box to the boolean function that flips the "download_info" thread on and off
         self.input_url.textChanged.connect(self.call_info_thread)
         
+        # Grab video formats from combo box
+        self.video_formats = [self.combo_video_formats.itemText(i) for i in range(self.combo_video_formats.count())]
+
+        # Grab audio formats from combo box
+        self.audio_formats = [self.combo_audio_formats.itemText(i) for i in range(self.combo_audio_formats.count())]
+        # Check Audio Only Checkbox
+        self.check_audioonly.toggled.connect(self.toggle_formats)
+
+
         # The Variable needInfo, used to test if the line has changed without freezing
         self.needInfo = False
 
@@ -32,19 +52,77 @@ class Window(QMainWindow, Ui_MainWindow):
         self.info_thread = Thread(target=self.live_info_thread)
         self.info_thread.setDaemon(True) # Die on main Death
         self.info_thread.start()
+
+        self.info = {}
         
-        
-    # Get input_url text
-    def get_url(self):
-        return self.input_url.text()
+        self.info_ydl_opts = {
+            'progress_hooks': [self.progress_hook]
+        }
+        self.video_ydl_opts = {
+            'format': 'best',
+            'ffmpeg_location': BIN,
+            'progress_hooks': [self.progress_hook],
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': self.combo_video_formats.currentText(),
+        }],
+        }
+        self.audio_ydl_opts = {
+            'format': 'bestaudio/best',
+            'ffmpeg_location': BIN,
+            'progress_hooks': [self.progress_hook],
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': self.combo_audio_formats.currentText(),
+                'preferredquality': '192',
+        }],
+        }
+
+        self.download_options = {
+
+        }
+
+    def toggle_formats(self):
+        if self.combo_video_formats.isHidden():
+            self.combo_video_formats.show()
+        else:
+            self.combo_video_formats.hide()
 
     # Test Function, no one purpose, change as needed to test things
     def test(self):
         pass
+    
+    def download_button(self):
+        if self.check_audioonly.isChecked() == False:
+            self.download_thread_maker(self.download_vid)
+            self.progress.show()
+        else:
+            self.download_thread_maker(self.download_audio)
+            self.progress.show()
+            
 
-    # Download the URL
-    def download_url(self):
-        with ydl.YoutubeDL(self.ydl_opts) as yt:
+    def download_thread_maker(self,x):
+        self.download_thread = Thread(target=x)
+        self.download_thread.setDaemon(True) # Die on main Death
+        self.download_thread.start()
+
+    # Get input_url text
+    def get_url(self):
+        return self.input_url.text()
+
+    
+
+    # Download the URL as Audio
+    def download_audio(self):
+        self.audio_ydl_opts['postprocessors'][0]['preferredcodec'] = self.combo_audio_formats.currentText()
+        with ydl.YoutubeDL(self.audio_ydl_opts) as yt:
+            yt.download([self.get_url()])
+        
+
+    # Download the URL as Video
+    def download_vid(self):
+        self.video_ydl_opts['postprocessors'][0]['preferedformat'] = self.combo_video_formats.currentText()
+        with ydl.YoutubeDL(self.video_ydl_opts) as yt:
             yt.download([self.get_url()])
 
     # Link to the "if text edited" signal, acting as a link to the "live_info_thread" which 
@@ -53,14 +131,39 @@ class Window(QMainWindow, Ui_MainWindow):
     def call_info_thread(self):
         self.needInfo = True
         
+    def get_vid_save_file(self):
+        dialog = QFileDialog()
+        name = QFileDialog.getSaveFileName(self, 'Save File',str(self.info['title']),'.'+self.combo_video_formats.currentText())
+        print(name)
+        return name#.split('.')[0]
+    
+    def get_audio_save_file(self):
+        dialog = QFileDialog()
+        name = QFileDialog.getSaveFileName(self, 'Save File',str(self.info['title']),'.'+self.combo_audio_formats.currentText())
+        print(name)
+        return name#.split('.')[0]
+
+    def progress_hook(self,x):
+        pass
+    #    print(x['status'])
+    #    if x['status'] == 'finished':
+    #        #file_tuple = os.path.split(os.path.abspath(x['filename']))
+    #        #print("Done downloading {}".format(file_tuple[1]))
+    #        self.progress.hide()
+    #        self.progressBar.setValue(float(0))
+    #    if x['status'] == 'downloading':
+    #        p = x['_percent_str']
+    #        p = p.replace('%','')
+    #        self.progressBar.setValue(float(p))
+    #        #print(x['filename'], x['_percent_str'], x['_eta_str'])
 
     # Receive the entire dictionary of information from the web address for use in display frame
     def download_info(self):
-        with ydl.YoutubeDL(self.ydl_opts) as yt:
+        with ydl.YoutubeDL(self.info_ydl_opts) as yt:
             try:
                 return yt.extract_info(self.get_url(),download=False)
             except Exception as e:
-                pass
+                print(e)
     
     
     # Forever looping thread to call information gathering 
@@ -75,17 +178,17 @@ class Window(QMainWindow, Ui_MainWindow):
     # Also calls "get_thumbnail_preview to display thumbnail preview at the same time as the other information"
     def update_info(self):
         try:
-            info = self.download_info()
+            self.info = self.download_info()
             # Get Thumbnail preview
-            self.img_thumbnail_display.setPixmap(self.get_thumbnail_preview(info['thumbnails'][0]['url']))
+            self.get_thumbnail_preview(self.info['thumbnails'][0]['url'])
             # Get Video Name
-            self.label_var_name.setText(info['title'])
+            self.label_var_name.setText(self.info['title'])
             # Get Video Length
-            self.label_var_duration.setText(str(datetime.timedelta(seconds=info['duration'])))
+            self.label_var_duration.setText(str(datetime.timedelta(seconds=self.info['duration'])))
             # Get Video Description
-            self.label_var_description.setText(info['description'])
+            self.label_var_description.setText(self.info['description'])
         except Exception as e:
-            pass
+            print(e)
         
     # Retrieve the thumbnail via the url provided in the info dictionary, 
     # and display it to the proper label as a pixmap
@@ -96,7 +199,7 @@ class Window(QMainWindow, Ui_MainWindow):
             img.save(bytes_img, format='JPEG')
             qimg = QImage()
             qimg.loadFromData(bytes_img.getvalue())
-            return QPixmap.fromImage(qimg)
+            self.img_thumbnail_display.setPixmap(QPixmap.fromImage(qimg))
         # Get link from url
         data = requests.get(url).content
         # Save link in Image
@@ -104,6 +207,7 @@ class Window(QMainWindow, Ui_MainWindow):
         # Send Image to function to be converted to pixmap
         img2pixmap(img)
 
+    
 
 
 
